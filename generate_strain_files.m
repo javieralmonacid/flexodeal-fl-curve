7bd6ada2a9e40d24ff15b83d9efdfd33f832bdf0
Javier Almonacid <almonacid.javier@gmail.com>
2025-03-14 21:15:52 -0700

    Move files strain files and bash scripts to flexodeal folder because of relative/absolute path issues with markers.dat

--- generate_strain_files.m
+++ generate_strain_files.m
@@ -5,7 +5,7 @@ strain = stretch - 1;
 
 % Isometric activation (lengthen, then activate)
 for k = 1:length(strain)
-    fid = fopen(['strains_len_act/control_points_strain_',num2str(k),'.dat'],'w');
+    fid = fopen(['flexodeal-0.2.3/strains_len_act/control_points_strain_',num2str(k),'.dat'],'w');
     fprintf(fid, '0.0 0.0\n');
     fprintf(fid, '0.2 %.2f\n', strain(k));
     fprintf(fid, '0.4 %.2f\n', strain(k));
@@ -14,7 +14,7 @@ end
 
 % Active lengthening (activate, then lengthen)
 for k = 1:length(strain)
-    fid = fopen(['strains_act_len/control_points_strain_',num2str(k),'.dat'],'w');
+    fid = fopen(['flexodeal-0.2.3/strains_act_len/control_points_strain_',num2str(k),'.dat'],'w');
     fprintf(fid, '0.0 0.0\n');
     fprintf(fid, '0.2 0.0\n');
     fprintf(fid, '0.4 %.2f\n', strain(k));
